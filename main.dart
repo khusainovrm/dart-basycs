8caa389519382a18dc2c2c40f169d56da49c107b
Rinat Khusainov <khusainovrm@yandex.ru>
2021-03-08 20:37:07 +0300

    first steps

--- main.dart
+++ main.dart
@@ -2,7 +2,25 @@ void printInteger(int aNumber) {
   print('aNumber $aNumber');
 }
 
+const outsideNumber = 42;
+const a = 2045;
+const b = 2017;
+const plantes = ['Earht', 'Pluton', "Neptun"];
+var start = 1;
+
 void main() {
-  var num = 42;
-  printInteger(num);
+  // const num = 42;
+  printInteger(outsideNumber);
+
+  for (var planet in plantes) {
+    print(planet);
+  }
+
+  if (a > 2016) {
+    print('This is huge, $a');
+  }
+
+  for (start; start <= 42; start++) {
+    print('$start');
+  }
 }
