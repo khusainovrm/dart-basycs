6a63835836e59a8aa543ef420aeb428b5dd9a5e6
Rinat Khusainov <khusainovrm@yandex.ru>
2021-03-10 22:22:36 +0300

    basics

--- main.dart
+++ main.dart
@@ -3,24 +3,41 @@ void printInteger(int aNumber) {
 }
 
 const outsideNumber = 42;
-const a = 2045;
+var a = 2045;
 const b = 2017;
-const plantes = ['Earht', 'Pluton', "Neptun"];
+const planets = ['Eath', 'Pluton', "Neptun"];
 var start = 1;
+var counter = 0;
+
+int fibonacci(int n) {
+  if (n == 0 || n == 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
 
 void main() {
+  // planets.forEach(print);
+  // planets.forEach((planet) => print(planet));
+  planets.where((element) => element.contains('Eath')).forEach(print);
+
   // const num = 42;
-  printInteger(outsideNumber);
+  // printInteger(outsideNumber);
 
-  for (var planet in plantes) {
-    print(planet);
-  }
+  // for (var planet in planets) {
+  //   print(planet);
+  // }
 
-  if (a > 2016) {
-    print('This is huge, $a');
-  }
+  // if (a > 2016) {
+  //   print('This is huge, $a');
+  // }
 
-  for (start; start <= 42; start++) {
-    print('$start');
-  }
+  // for (start; start <= 42; start++) {
+  //   print('$start');
+  // }
+
+  // var result = fibonacci(8);
+  // while (a < 2055) {
+  //   print(a);
+  //   a++;
+  // }
+  // print(result);
 }
