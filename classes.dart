14bc71c8a115d929300d078cc40e6bb9f1bee6ab
Rinat Khusainov <khusainovrm@yandex.ru>
2021-03-14 21:10:45 +0300

    1

--- classes.dart
+++ classes.dart
@@ -2,13 +2,19 @@ class Paladin {
   num? level;
   num? health;
 
+  // Paladin({this.level, this.health});
   Paladin({this.level, this.health});
+  Paladin.holy({this.level}){
+    this.health = 200;
+  }
 
   @override
   String toString() => 'Paladin has $level level and $health health';
 }
 
 void main(){
-  Paladin pal = Paladin(level:19, health: 1);
+  Paladin pal = Paladin(level:19, health:100);
+  Paladin hpal = Paladin.holy(level: 20);
   print(pal);
+  print(hpal);
 }
