6ff64482cab1c41d5cc0333be6140c7072c9798d
Rinat Khusainov <khusainovrm@yandex.ru>
2021-03-16 11:51:54 +0300

    async

--- rinat_project/bin/rinat_project.dart
+++ rinat_project/bin/rinat_project.dart
@@ -1,12 +1,20 @@
 import 'package:rinat_project/api.dart' as api;
 
-Future<String> task() async {
-  return await Future.delayed(Duration(seconds: 2), () => 'Task is completed');
+Future<String> dalayedUrl(String url) async {
+  return await Future.delayed(Duration(seconds: 1), () => url);
 }
 
 void main(List<String> arguments) async {
   const url = 'http://77.246.159.150:8000/api/v1/shared/reactions';
-  var result = await api.get(url);
-  print(result);
-  print(await task());
+  const roleUrl = 'http://77.246.159.150:8000/api/v1/shared/roles';
+
+  try {
+    var computedUrl = await dalayedUrl(url);
+    var result = await api.get(computedUrl);
+    print(result);
+  } catch (error) {
+    print(error);
+  }
+
+  await api.get(roleUrl).then(print).catchError(print);
 }
